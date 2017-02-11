c429fa09eeca34d11c212b6545430a95866090b8
Jean Prunneaux <jean@prunneaux.com>
2017-02-11 16:16:28 +0100

    Remove macOS unsupported error code

--- test_core_fs.sh
+++ test_core_fs.sh
@@ -3,12 +3,7 @@
 # get fstype
 tb_test -c 1 lb_df_fstype
 tb_test -c 2 lb_df_fstype notAfile
-
-if [ "$(lb_detect_os)" == "macOS" ] ; then
-	tb_test -c 4 lb_df_fstype "$0"
-else
-	tb_test lb_df_fstype "$0"
-fi
+tb_test lb_df_fstype "$0"
 
 
 # get space left
@@ -26,12 +21,7 @@ tb_test lb_df_mountpoint "$0"
 # get disk UUID
 tb_test -c 1 lb_df_uuid
 tb_test -c 2 lb_df_uuid notAfile
-
-if [ "$(lb_detect_os)" == "macOS" ] ; then
-	tb_test -c 4 lb_df_uuid "$0"
-else
-	tb_test lb_df_uuid "$0"
-fi
+tb_test lb_df_uuid "$0"
 
 
 # avoid skipping tests if last failed
