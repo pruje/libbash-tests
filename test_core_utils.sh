afbb813a49200675691706f297ce213909d992e9
Jean Prunneaux <jean@prunneaux.com>
2017-03-30 00:23:30 +0200

    Add tests for lb_generate_password()

--- test_core_utils.sh
+++ test_core_utils.sh
@@ -4,6 +4,12 @@
 tb_test lb_detect_os
 
 
+# generate password
+tb_test -c 1 lb_generate_password aaa
+tb_test lb_generate_password
+tb_test lb_generate_password 8
+
+
 # send email
 tb_test lb_email -s Test junk@example.com "This is a test email."
 
