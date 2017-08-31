c923c9909e6cc6e9e7e85c111fd9ff80f7d08f90
Jean Prunneaux <jean@prunneaux.com>
2017-08-31 19:20:49 +0200

    Add tests for lb_import_config

--- libbash-core/test_utils.sh
+++ libbash-core/test_utils.sh
@@ -31,5 +31,62 @@ tb_test lb_generate_password 8
 #tb_test lb_email -s Test junk@example.com "This is a test email."
 
 
+# import config files
+tb_test -c 1 lb_import_config
+tb_test -c 1 lb_import_config badConfigFile
+
+# initialize config values
+boolean1=true
+boolean2=true
+
+# create a config file
+configfile="./testbash_testconfig.conf"
+cat > "$configfile" <<EOF
+# this is a comment
+
+boolean1=false
+   boolean2 = false
+int1=99
+   int2 = 99
+arr1=("opt 1" "opt 2")
+   arr2 = ("opt 1" "opt 2")
+str1="hello world"
+   str2  =  "hello world"
+EOF
+
+# import config
+tb_test -i lb_import_config -e "$configfile"
+
+# adding bad parameters
+cat >> "$configfile" <<EOF
+this is non sense
+EOF
+
+# test import with errors
+tb_test -c 3 lb_import_config -e "$configfile"
+
+# adding shell injection
+cat >> "$configfile" <<EOF
+unsecure="\$(echo "injection")"
+EOF
+
+# test import in unsecure mode
+tb_test -c 4 lb_import_config -e "$configfile"
+tb_test -c 3 -i lb_import_config -e -u "$configfile"
+
+# test values
+tb_test -v -r false $boolean1
+tb_test -v -r $boolean1 $boolean2
+tb_test -v -r 99 $int1
+tb_test -v -r $int1 $int2
+tb_test -v -r "opt 1" "${arr1[0]}"
+tb_test -v -r "${arr1[1]}" "${arr2[1]}"
+tb_test -v -r "hello world" "$str1"
+tb_test -v -r "$str1" "$str2"
+tb_test -v -r "injection" "$unsecure"
+
+# delete test config file
+rm -f "$configfile"
+
 # avoid skipping tests if last failed
 return 0
