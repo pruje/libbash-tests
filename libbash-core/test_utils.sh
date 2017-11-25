257fd5cf1136737a04120319d424663ed991d5f6
Jean Prunneaux <jean@prunneaux.com>
2017-11-25 14:59:17 +0100

    Add tests for lb_email + move config tests in separate file

--- libbash-core/test_utils.sh
+++ libbash-core/test_utils.sh
@@ -39,108 +39,30 @@ tb_test lb_generate_password 8
 
 
 # send email
-# UNCOMMENT THIS LINE TO TEST IT
-#tb_test lb_email -s Test junk@example.com "This is a test email."
+# SET a destination below to enable tests
+tb_email_dest=""
 
+if [ -n "$tb_email_dest" ] ; then
+	tb_email_prefix="[libbash.sh unit tests]"
+	tb_email_attachment="$(dirname "$(lb_realpath "$BASH_SOURCE")")/mail attachment.txt"
 
-# read/import config files
-tb_test -c 1 lb_read_config
-tb_test -c 1 lb_read_config badConfigFile
-tb_test -c 1 lb_import_config
-tb_test -c 1 lb_import_config badConfigFile
+	tb_test -c 1 lb_email
+	tb_test -c 1 lb_email missingTheText
 
-# initialize config values
-boolean1=true
-boolean2=true
+	tb_test -n "Simple text email" lb_email -s "$tb_email_prefix Simple text email" $tb_email_dest "This is a simple text email."
 
-# create a config file
-configfile="./testbash_testconfig.conf"
-cat > "$configfile" <<EOF
-# this is a comment
-; this is another comment
+	tb_test -n "HTML/txt email" lb_email -s "$tb_email_prefix Multipart HTML/TXT email" --html \
+		"This is a multipart email sent by <a href='https://github.com/pruje/libbash.sh'>libbash.sh</a>, and you are reading the HTML part." \
+		$tb_email_dest "This is a multipart email, and you are reading the text part."
 
-[global]
-boolean1=false
-   boolean2 = false
-int1=99
-   int2 = 99
-arr1=("opt 1" "opt 2")
-   arr2 = ("opt 1" "opt 2")
-str1="hello world"
-   str2  =  "hello world"
+	tb_test -n "Simple text email with attachment" lb_email -s "$tb_email_prefix Simple text email with attachment" -a "$tb_email_attachment" \
+		$tb_email_dest "This is a simple text email and you should have an attachment."
 
-[part2]
-int1=88
-EOF
-
-# import config
-tb_test -i lb_import_config -e "$configfile"
-echo $?
-# adding bad parameters
-cat >> "$configfile" <<EOF
-this is non sense
-EOF
-
-# read config
-tb_test -i lb_read_config "$configfile"
-tb_test -n "Config file lines > 0" [ ${#lb_read_config[@]} -gt 0 ]
-if [ $? == 0 ] ; then
-	tb_test -n "Last line of config file" -v -r "this is non sense" ${lb_read_config[${#lb_read_config[@]}-1]}
+	tb_test -n "HTML/txt email with attachment" lb_email -s "$tb_email_prefix Multipart HTML/TXT email with attachment" -a "$tb_email_attachment" --html \
+		"This is a multipart email sent by <a href='https://github.com/pruje/libbash.sh'>libbash.sh</a>, and you are reading the HTML part.<br/>You should have an attachment." \
+		$tb_email_dest "This is a multipart email, and you are reading the text part, and you should have an attachment."
 fi
 
-# read section
-tb_test -i lb_read_config -s global "$configfile"
-tb_test -n "Last line of global section" -v -r "str2 = \"hello world\"" ${lb_read_config[${#lb_read_config[@]}-1]}
-
-# test import with errors
-tb_test -c 3 lb_import_config -e "$configfile"
-
-tb_test -v -r 88 $int1
-
-# adding shell injection
-cat >> "$configfile" <<EOF
-unsecure="\$(echo "injection")"
-EOF
-
-# test import in unsecure mode
-tb_test -c 4 lb_import_config -e "$configfile"
-tb_test -c 3 -i lb_import_config -e -u "$configfile"
-tb_test -v -r "injection" "$unsecure"
-
-# test loading section
-tb_test -i lb_import_config -s global "$configfile"
-
-# test values
-tb_test -v -r false $boolean1
-tb_test -v -r $boolean1 $boolean2
-tb_test -v -r 99 $int1
-tb_test -v -r $int1 $int2
-tb_test -v -r "opt 1" "${arr1[0]}"
-tb_test -v -r "${arr1[1]}" "${arr2[1]}"
-tb_test -v -r "hello world" "$str1"
-tb_test -v -r "$str1" "$str2"
-
-# set values
-tb_test -c 1 lb_set_config
-tb_test -c 1 lb_set_config badConfigFile
-tb_test -c 1 lb_set_config "$configfile"
-tb_test -c 3 lb_set_config --strict "$configfile" badParameter value
-tb_test lb_set_config "$configfile" int1 101
-
-# test set values
-tb_test -i lb_import_config "$configfile"
-tb_test -v -r 101 $int1
-
-# set value in section
-tb_test lb_set_config -s global "$configfile" int1 101
-tb_test -i lb_import_config -s global "$configfile"
-tb_test -v -r 101 $int1
-tb_test lb_set_config -s part2 "$configfile" int1 102
-tb_test -i lb_import_config -s part2 "$configfile"
-tb_test -v -r 102 $int1
-
-# delete test config file
-rm -f "$configfile"*
 
 # avoid skipping tests if last failed
 return 0
