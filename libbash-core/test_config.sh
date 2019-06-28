2ff209efb6354c8d38a4e793654b159991e3815c
Jean Prunneaux <jean@prunneaux.com>
2019-06-28 21:39:59 +0200

    Add filters test for lb_import_config

--- libbash-core/test_config.sh
+++ libbash-core/test_config.sh
@@ -31,6 +31,7 @@ int1=88
 EOF
 
 # import config
+tb_test -i -c 2 lb_import_config -e "$configfile" onlyThisParam
 tb_test -i lb_import_config -e "$configfile"
 
 # adding bad parameters
@@ -44,17 +45,17 @@ tb_test -i lb_read_config "$configfile" || res=$?
 
 if [ $res == 0 ] ; then
 	tb_test -n "Config file lines > 0" [ ${#lb_read_config[@]} -gt 0 ] && \
-	tb_test -n "Last line of config file" -v -r "this is non sense" ${lb_read_config[${#lb_read_config[@]}-1]}
+	tb_test -n "Last line of config file" -r "this is non sense" -v ${lb_read_config[${#lb_read_config[@]}-1]}
 fi
 
 # read section
 tb_test -i lb_read_config -s global "$configfile"
-tb_test -n "Last line of global section" -v -r "str2 = \"hello world\"" ${lb_read_config[${#lb_read_config[@]}-1]}
+tb_test -n "Last line of global section" -r "str2 = \"hello world\"" -v ${lb_read_config[${#lb_read_config[@]}-1]}
 
 # test import with errors
 tb_test -c 3 lb_import_config -e "$configfile"
 
-tb_test -v -r 88 $int1
+tb_test -r 88 -v $int1
 
 # adding shell injection
 cat >> "$configfile" <<EOF
@@ -64,20 +65,20 @@ EOF
 # test import in unsecure mode
 tb_test -c 4 lb_import_config -e "$configfile"
 tb_test -c 3 -i lb_import_config -e -u "$configfile"
-tb_test -v -r "injection" "$unsecure"
+tb_test -r "injection" -v "$unsecure"
 
 # test loading section
 tb_test -i lb_import_config -s global "$configfile"
 
 # test values
-tb_test -v -r false $boolean1
-tb_test -v -r $boolean1 $boolean2
-tb_test -v -r 99 $int1
-tb_test -v -r $int1 $int2
-tb_test -v -r "opt 1" "${arr1[0]}"
-tb_test -v -r "${arr1[1]}" "${arr2[1]}"
-tb_test -v -r "hello world" "$str1"
-tb_test -v -r "$str1" "$str2"
+tb_test -r false -v $boolean1
+tb_test -r $boolean1 -v $boolean2
+tb_test -r 99 -v $int1
+tb_test -r $int1 -v $int2
+tb_test -r "opt 1" -v "${arr1[0]}"
+tb_test -r "${arr1[1]}" -v "${arr2[1]}"
+tb_test -r "hello world" -v "$str1"
+tb_test -r "$str1" -v "$str2"
 
 # set values
 tb_test -c 1 lb_set_config
